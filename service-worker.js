6e0c1b0f0b1176d4fd2dd438341e5f420a50ac02
nzardetto <zardetto22@gmail.com>
2026-05-07 16:08:15 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'toolbox-v1';
+const CACHE_NAME = 'toolbox-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './script.js',
-  './manifest.json'
+  './manifest.json',
+  './icon-512.png'
 ];
 
-// Instala o Service Worker e armazena os arquivos no cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Responde com o cache quando estiver offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
